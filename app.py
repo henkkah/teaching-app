a04264c41c739cb2ce125dcd9c4bb2c9281b42f3
henkkaha <henrik.harjula@outlook.com>
2021-05-28 09:09:51 +0300

    creating course functionality created

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def login():
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
     if user == None: # wrong username
-        return render_template("wrong-username.html")
+        return render_template("error-username-incorrect.html")
     else:
         password_hash = user[0]
         if check_password_hash(password_hash, password): # correct username and password
@@ -36,7 +36,7 @@ def login():
             else: # student
                 return redirect("/student")
         else: # wrong password
-            return render_template("wrong-password.html")
+            return render_template("error-password-incorrect.html")
 
 @app.route("/newuser")
 def newuser():
@@ -47,7 +47,7 @@ def createuser():
     username = request.form["username"]
     password = request.form["password"]
     if "teacher" not in request.form:
-        return render_template("role-notchosen.html")
+        return render_template("error-role-not-chosen.html")
     else:
         teacher = request.form["teacher"]
     
@@ -56,11 +56,11 @@ def createuser():
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
     if user != None or username.strip() == "": # username in use or empty
-        return render_template("username-inuse.html")
+        return render_template("error-username-in-use.html")
     
     # Check password
     if password.strip() == "":
-        return render_template("password-empty.html")
+        return render_template("error-password-empty.html")
     
     password_hash = generate_password_hash(password)
     sql = "INSERT INTO users (username, password, teacher) VALUES (:username, :password, :teacher)"
@@ -72,7 +72,6 @@ def createuser():
         return redirect("/teacher")
     else: # student
         return redirect("/student")
-
 
 
 @app.route("/teacher")
@@ -89,9 +88,56 @@ def logout():
     return redirect("/")
 
 
+@app.route("/newcourse")
+def newcourse():
+    return render_template("newcourse.html")
 
+@app.route("/createcourse", methods=["POST"])
+def createcourse():
+    coursename = request.form["coursename"]
+    coursecode = request.form["coursecode"]
+    if "language" not in request.form:
+        return render_template("error-language-not-chosen.html")
+    else:
+        language = request.form["language"]
+    if "level" not in request.form:
+        return render_template("error-level-not-chosen.html")
+    else:
+        level = request.form["level"]
+    ects = request.form["ects"]
+    limit = request.form["limit"]
+    
+    # Check coursename
+    if coursename.strip() == "":
+        return render_template("error-coursename-not-given.html")
+    # Check coursecode
+    sql = "SELECT code FROM courses WHERE code=:coursecode"
+    result = db.session.execute(sql, {"coursecode":coursecode})
+    code = result.fetchone()
+    if code != None or coursecode.strip() == "": # coursecode in use or empty
+        return render_template("error-coursecode-in-use.html")
+    # Check ects
+    try:
+        ects = int(ects)
+        if ects < 0:
+            return render_template("error-ects-invalid.html")
+    except:
+        return render_template("error-ects-invalid.html")
+    # Check limit
+    try:
+        limit = int(limit)
+        if limit < 0 or limit > 100:
+            return render_template("error-limit-invalid.html")
+    except:
+        return render_template("error-limit-invalid.html")
 
-
+    # Add new course to db
+    teacher_id = db.session.execute("SELECT id FROM users WHERE username=:username", {"username":session["username"]}).fetchone()[0]
+    sql = "INSERT INTO courses (code, name, teacher_id, lang, lev, ects, lim, visible, deleted) VALUES (:coursecode, :coursename, :teacher_id, :language, :level, :ects, :limit, :visible, :deleted)"
+    db.session.execute(sql, {"coursecode":coursecode, "coursename":coursename, "teacher_id":teacher_id, "language":language, "level":level, "ects":ects, "limit":limit, "visible":"0", "deleted":"0"})
+    db.session.commit()
+    
+    return redirect("/teacher")
 
 
 
