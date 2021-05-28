2e5a989d6c1deb4d6e4e632ab4aa6b775de7a21d
henkkaha <henrik.harjula@outlook.com>
2021-05-28 11:45:51 +0300

    course parameters can be modified by teacher

--- app.py
+++ app.py
@@ -80,19 +80,19 @@ def createuser():
 @app.route("/teacher")
 def teacher():
     teacher_id = db.session.execute("SELECT id FROM users WHERE username=:username", {"username":session["username"]}).fetchone()[0]
-    sql1 = "SELECT code, name, lang, lev, ects, lim FROM courses WHERE teacher_id=:teacher_id AND visible=:visible AND deleted=:deleted"
+    sql1 = "SELECT id, code, name, lang, lev, ects, lim FROM courses WHERE teacher_id=:teacher_id AND visible=:visible AND deleted=:deleted"
     result1 = db.session.execute(sql1, {"teacher_id":teacher_id, "visible":"1", "deleted":"0"})
-    sql2 = "SELECT code, name, lang, lev, ects, lim FROM courses WHERE teacher_id=:teacher_id AND visible=:visible AND deleted=:deleted"
+    sql2 = "SELECT id, code, name, lang, lev, ects, lim FROM courses WHERE teacher_id=:teacher_id AND visible=:visible AND deleted=:deleted"
     result2 = db.session.execute(sql1, {"teacher_id":teacher_id, "visible":"0", "deleted":"0"})
     
     visiblecourses_str = []
     for result in result1:
-        string = result[0] + " " + result[1] + " (" + language_mapping[result[2]] + ", " + level_mapping[result[3]] + ", " + str(result[4]) + " ECTS, " + str(result[5]) + " % to completion)"
-        visiblecourses_str.append(string)
+        string = result[1] + " " + result[2] + " (" + language_mapping[result[3]] + ", " + level_mapping[result[4]] + ", " + str(result[5]) + " ECTS, " + str(result[6]) + " % to completion)"
+        visiblecourses_str.append((string, result[0]))
     hiddencourses_str = []
     for result in result2:
-        string = result[0] + " " + result[1] + " (" + language_mapping[result[2]] + ", " + level_mapping[result[3]] + ", " + str(result[4]) + " ECTS, " + str(result[5]) + " % to completion)"
-        hiddencourses_str.append(string)
+        string = result[1] + " " + result[2] + " (" + language_mapping[result[3]] + ", " + level_mapping[result[4]] + ", " + str(result[5]) + " ECTS, " + str(result[6]) + " % to completion)"
+        hiddencourses_str.append((string, result[0]))
     visiblecourses_str.sort()
     hiddencourses_str.sort()
     
@@ -158,6 +158,83 @@ def createcourse():
     db.session.commit()
     
     return redirect("/teacher")
+
+
+@app.route("/changecourse/<int:id>")
+def changecourse(id):
+    teacher_id = db.session.execute("SELECT id FROM users WHERE username=:username", {"username":session["username"]}).fetchone()[0]
+    sql = "SELECT code, name, lang, lev, ects, lim FROM courses WHERE teacher_id=:teacher_id AND id=:id"
+    result = db.session.execute(sql, {"teacher_id":teacher_id, "id":id}).fetchone()
+    
+    
+    if result[2] == "SWE":
+        if result[3] == "ADV":
+            return render_template("changecourse-i.html", id=id, coursename=result[1], coursecode=result[0], ects=result[4], limit=result[5])
+        elif result[3] == "INT":
+            return render_template("changecourse-h.html", id=id, coursename=result[1], coursecode=result[0], ects=result[4], limit=result[5])
+        else:
+            return render_template("changecourse-g.html", id=id, coursename=result[1], coursecode=result[0], ects=result[4], limit=result[5])
+    elif result[2] == "FIN":
+        if result[3] == "ADV":
+            return render_template("changecourse-f.html", id=id, coursename=result[1], coursecode=result[0], ects=result[4], limit=result[5])
+        elif result[3] == "INT":
+            return render_template("changecourse-e.html", id=id, coursename=result[1], coursecode=result[0], ects=result[4], limit=result[5])
+        else:
+            return render_template("changecourse-d.html", id=id, coursename=result[1], coursecode=result[0], ects=result[4], limit=result[5])
+    else:
+        if result[3] == "ADV":
+            return render_template("changecourse-c.html", id=id, coursename=result[1], coursecode=result[0], ects=result[4], limit=result[5])
+        elif result[3] == "INT":
+            return render_template("changecourse-b.html", id=id, coursename=result[1], coursecode=result[0], ects=result[4], limit=result[5])
+        else:
+            return render_template("changecourse-a.html", id=id, coursename=result[1], coursecode=result[0], ects=result[4], limit=result[5])
+
+@app.route("/modifycourse/<int:id>", methods=["POST"])
+def modifycourse(id):
+    coursename = request.form["coursename"]
+    coursecode = request.form["coursecode"]
+    language = request.form["language"]
+    level = request.form["level"]
+    ects = request.form["ects"]
+    limit = request.form["limit"]
+    
+    # Check coursename
+    if coursename.strip() == "":
+        return render_template("error-mod-coursename-not-given.html", id=id)
+    # Check coursecode
+    old_code = db.session.execute("SELECT code FROM courses WHERE id=:id", {"id":id}).fetchone()[0]
+    sql = "SELECT code FROM courses WHERE code=:coursecode"
+    result = db.session.execute(sql, {"coursecode":coursecode})
+    code = result.fetchone()
+    if (code != None and code[0] != old_code) or coursecode.strip() == "": # coursecode in use or empty
+        return render_template("error-mod-coursecode-in-use.html", id=id)
+    # Check ects
+    try:
+        ects = int(ects)
+        if ects < 0:
+            return render_template("error-mod-ects-invalid.html", id=id)
+    except:
+        return render_template("error-mod-ects-invalid.html", id=id)
+    # Check limit
+    try:
+        limit = int(limit)
+        if limit < 0 or limit > 100:
+            return render_template("error-mod-limit-invalid.html", id=id)
+    except:
+        return render_template("error-mod-limit-invalid.html", id=id)
+
+    # Modify course in db
+    teacher_id = db.session.execute("SELECT id FROM users WHERE username=:username", {"username":session["username"]}).fetchone()[0]
+    sql = "UPDATE courses SET code=:coursecode, name=:coursename, lang=:language, lev=:level, ects=:ects, lim=:limit WHERE teacher_id = :teacher_id and id = :id"
+    db.session.execute(sql, {"coursecode":coursecode, "coursename":coursename, "language":language, "level":level, "ects":ects, "limit":limit, "teacher_id":teacher_id, "id":id})
+    db.session.commit()
+    
+    return redirect("/teacher")
+
+
+
+
+
 
 
 
